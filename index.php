bed16829db81db8b4aa5fda55224ad4b0b76d56e
andrewcas82 <67315875+andrewcas82@users.noreply.github.com>
2020-11-09 19:57:19 +0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,79 @@
+
+<!DOCTYPE html>
+<html lang="en" >
+    
+    <head>
+        <meta charset="utf-8">
+        <title>E-Shop</title>
+
+    </head>
+
+    <body>
+        <!-- Header -->
+        <header>
+            <!-- Logo -->
+            <div id="logo" >
+                <a href="index.php">
+                    E-Shop
+                </a>
+            </div>
+
+            <!-- Menu -->            
+            <nav id="nav" >
+                <ul>
+                    <li>
+                        <a href="">Inicio</a>
+                    </li>
+                    <li>
+                        <a href="">Categoria</a>
+                    </li>
+                    <li>
+                        <a href="">Carrto</a>
+                    </li>
+                    <li>
+                        <a href="">Lista de deseo</a>
+                    </li>
+                    <li>
+                        <a href="">Acerca de...</a>
+                    </li>
+                    <li>
+                        <a href="">Contacto</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+
+        <div id="container" >
+            <!-- side bar -->
+            <aside id="sidebar" >
+                <div id="login" >
+                    <h3>Identificate</h3>
+                    <form action="login.php" method="post">
+                        <label for="email">Email</label>
+                        <input type="email" name="email" >
+
+                        <label for="password">Contrasena</label>
+                        <input type="password" name="password"> 
+
+                        <input type="submit" value="Entrar">  
+                    </form>
+                </div>
+            </aside>
+        </div>
+        
+        
+
+        <!-- Side Bar -->
+
+
+        <!-- Main Container-->
+
+
+        <!-- Footer -->
+    </body>
+
+<!-- -->
+
+
+
+</html>
